c1efecc848f2e028ddbc45d992deb5f1741d2391
DmytroPundyk <dimonpund@gmail.com>
2022-02-14 18:59:15 +0200

    header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,21 @@
 import { FaAddressBook } from 'react-icons/fa';
+import styled from 'styled-components';
+
+const Link = styled.a`
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+    color: yellowgreen;
+  }
+`;
 
 const Header = () => {
   return (
     <>
       <h1>
-        PHONEBOOK <FaAddressBook />
+        <Link href="/">
+          PHONEBOOK <FaAddressBook />
+        </Link>
       </h1>
     </>
   );
